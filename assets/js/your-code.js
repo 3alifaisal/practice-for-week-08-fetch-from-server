55647471294060e96c4d37c8107b94b72289eba3
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-02-20 20:27:07 +0100

    finished practice
    learned a lot about headers and fetch itself and also looked into the entire code and how it works.

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,67 @@
 export function getAllDogs() {
     // Your code here
+    return fetch("/dogs")
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    return fetch("/dogs/2")
 }
 
 export function postNewDog() {
     // Your code here
+    const url = "/dogs";
+    const headers = { "Content-Type": "application/x-www-form-urlencoded" };
+    // Use the URLSearchParams API to format your body as shown below
+    const body = new URLSearchParams({
+        name: "kira",
+        age: "99"
+    });
+
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    };
+
+  return   fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
      // Your code here
+    // Your code here    
+    const url = "/dogs";
+    const headers = { 
+        "Content-Type": "application/x-www-form-urlencoded" };
+    // Use the URLSearchParams API to format your body as shown below
+    const body = new URLSearchParams({
+        name,
+        age
+    });
+
+    const options = {
+        method: "POST",
+        headers: headers,
+        body: body
+    };
+    return fetch(url, options);
+
 }
 
 export function deleteDog(id) {
       // Your code here
+    const url = "/dogs/" + id + "/delete";
+    const headers = {
+        "AUTH": 'ckyut5wau0000jyv5bsrud90y'
+    };
+
+
+   
+
+    const options = {
+        method: "POST",
+        headers: headers,
+      
+    };
+    return fetch(url, options);
 }
